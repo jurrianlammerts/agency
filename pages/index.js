4c8458070cfda31b7794a6d5160b5203d6e49d2c
Jurrian <jurrianlammerts@hotmail.com>
2020-11-17 17:41:48 +0100

    Added Seo content

--- pages/index.js
+++ pages/index.js
@@ -55,7 +55,19 @@ function Home() {
     <div className="Home">
       <Head>
         <title>Agency.</title>
+        <meta
+          name="description"
+          content="Creating unique brands is what we do."
+        />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta property="og:title" content="Agency." key="ogtitle" />
+        <meta
+          property="og:description"
+          content="Creating unique brands is what we do."
+          key="ogdesc"
+        />
       </Head>
       <IntroOverlay />
       <Header />
