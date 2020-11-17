dbd82e58d3504f00ba3e8954634f95d5608b85d4
Jurrian <jurrianlammerts@hotmail.com>
2020-11-17 15:18:36 +0100

    Rewrote agency app in Next.js

---
+++ components/Banner.js
@@ -0,0 +1,26 @@
+import ArrowRight from '../components/icons/ArrowRight';
+
+export default function Banner() {
+  return (
+    <section className="main">
+      <div className="container">
+        <div className="row">
+          <h2>
+            <div className="line">
+              <span>Creating unique brands is</span>
+            </div>
+            <div className="line">
+              <span>what we do.</span>
+            </div>
+          </h2>
+          <div className="btn-row">
+            <a href="/">
+              More about us
+              <ArrowRight />
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ components/Cases.js
@@ -0,0 +1,53 @@
+import ArrowLeft from '../components/icons/ArrowLeft';
+import ArrowRight from '../components/icons/ArrowRight';
+
+const caseStudies = [
+  {
+    id: 1,
+    subtitle: 'Curology',
+    title: 'A custom formula for your skin’s unique needs',
+    img: 'curology-min',
+  },
+  {
+    id: 2,
+    subtitle: 'Yourspace',
+    title: 'Open space floor plans for you next venture',
+    img: 'yourspace-min',
+  },
+  {
+    id: 3,
+    subtitle: 'Lumin',
+    title: 'For your best look ever',
+    img: 'lumin-min',
+  },
+];
+
+export default function Cases() {
+  return (
+    <section className="cases">
+      <div className="container-fluid">
+        <div className="cases-navigation">
+          <div className="cases-arrow prev disabled">
+            <ArrowLeft />
+          </div>
+          <div className="cases-arrow next">
+            <ArrowRight />
+          </div>
+        </div>
+        <div className="row">
+          {caseStudies.map((caseItem) => (
+            <div className="case" key={caseItem.id}>
+              <div className="case-details">
+                <span>{caseItem.subtitle}</span>
+                <h2>{caseItem.title}</h2>
+              </div>
+              <div className="case-image">
+                {console.log(`../assets/${caseItem.img}.png`)}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
