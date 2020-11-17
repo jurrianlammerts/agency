4e91c5c222c7387b7c3cdb1075c06bbe92b064f7
Jurrian <jurrianlammerts@hotmail.com>
2020-11-17 17:24:21 +0100

    Added animations to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,67 @@
 import Head from 'next/head';
+import gsap from 'gsap';
 
 import { useEffect } from 'react';
 import Header from '../components/Header';
 import Banner from '../components/Banner';
 import Cases from '../components/Cases';
+import IntroOverlay from '../components/IntroOverlay';
 
 function Home() {
   useEffect(() => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    // Prevent flashing
+    gsap.to('body', 0, { css: { visibility: 'visible' } });
+
+    // Timeline
+    const tl = gsap.timeline();
+
+    tl.from('.line span', 1.8, {
+      y: 100,
+      ease: 'power4.out',
+      delay: 1,
+      skewY: 7,
+      stagger: {
+        amount: 0.3,
+      },
+    })
+      .to('.overlay-top', 1.6, {
+        height: 0,
+        ease: 'expo.inOut',
+        stagger: 1.4,
+      })
+      .to('.overlay-bottom', 1.6, {
+        width: 0,
+        ease: 'expo.inOut',
+        delay: -0.8,
+        stagger: {
+          amount: 0.4,
+        },
+      })
+      .to('.intro-overlay', 0, { css: { display: 'none' } })
+      .from('.case-image img', 1.6, {
+        scale: 1.4,
+        ease: 'expo.inOut',
+        delay: -2,
+        stagger: {
+          amount: 0.4,
+        },
+      });
   }, []);
 
   return (
-    <>
+    <div className="App">
       <Head>
         <title>Agency.</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <IntroOverlay />
       <Header />
       <Banner />
       <Cases />
-    </>
+    </div>
   );
 }
 
