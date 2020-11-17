81165fb03cf4c22d18911f0c934ae5f130c8805f
Jurrian <jurrianlammerts@hotmail.com>
2020-11-18 00:40:13 +0100

    Removed buggy slider

--- components/CasesSection.js
+++ components/CasesSection.js
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRef, useEffect } from 'react';
 import Slider from 'react-slick';
 
 import ArrowLeft from './icons/ArrowLeft';
@@ -24,81 +25,38 @@ const caseStudies = [
     title: 'A campaign for biking passionates',
     img: 'bike-min',
   },
-  {
-    id: 4,
-    subtitle: 'Lumin',
-    title: 'For your best look ever',
-    img: 'lumin-min',
-  },
-  {
-    id: 5,
-    subtitle: 'Era',
-    title: 'Communicating high tech sport garments',
-    img: 'sports-min',
-  },
 ];
 
 export default function Cases() {
-  var settings = {
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    // nextArrow: (
-    //   <div className="cases-arrow next">
-    //     <ArrowRight />
-    //   </div>
-    // ),
-    // prevArrow: (
-    //   <div className="cases-arrow prev disabled">
-    //     <ArrowLeft />
-    //   </div>
-    // ),
-    responsive: [
-      {
-        breakpoint: 1224,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-        },
-      },
-      {
-        breakpoint: 920,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-        },
-      },
-    ],
-  };
-
   return (
     <section className="cases">
-      <Slider {...settings} arrows={false}>
-        {caseStudies.map((caseItem) => (
-          <div className="case" key={caseItem.id}>
-            <div className="case-details">
-              <span>{caseItem.subtitle}</span>
-              <h2>{caseItem.title}</h2>
-              <div className="btn-row">
-                <Link href="/about" passHref>
-                  <a>
-                    Case <ArrowRight />
-                  </a>
-                </Link>
+      <div className="container-fluid">
+        {/* <div className="cases-navigation">
+          <div className="cases-arrow prev disabled">
+            <ArrowLeft />
+          </div>
+          <div className="cases-arrow next">
+            <ArrowRight />
+          </div>
+        </div> */}
+        <div className="row">
+          {caseStudies.map((caseItem) => (
+            <div className="case" key={caseItem.id}>
+              <div className="case-details">
+                <span>{caseItem.subtitle}</span>
+                <h2>{caseItem.title}</h2>
+              </div>
+              <div className="case-image">
+                <Image
+                  src={`/images/${caseItem.img}.png`}
+                  alt={caseItem.title}
+                  layout="fill"
+                />
               </div>
             </div>
-            <div className="case-image">
-              <Image
-                src={`/images/${caseItem.img}.png`}
-                alt={caseItem.title}
-                layout="fill"
-              />
-            </div>
-          </div>
-        ))}
-      </Slider>
+          ))}
+        </div>
+      </div>
     </section>
   );
 }
