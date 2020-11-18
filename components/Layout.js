d4708625544ab0e9d1908ddf06f425b560bca7db
Jurrian <jurrianlammerts@hotmail.com>
2020-11-18 17:04:29 +0100

    Changed name

--- components/Layout.js
+++ components/Layout.js
@@ -51,7 +51,7 @@ export default function Layout({ children, home }) {
       <Nav />
       <div className="App">
         <Head>
-          <title>Agency.</title>
+          <title>RDAM.</title>
           <meta
             name="description"
             content="Creating unique brands is what we do."
@@ -59,7 +59,7 @@ export default function Layout({ children, home }) {
           <link rel="icon" href="/favicon.ico" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
-          <meta property="og:title" content="Agency." key="ogtitle" />
+          <meta property="og:title" content="RDAM." key="ogtitle" />
           <meta
             property="og:description"
             content="Creating unique brands is what we do."
