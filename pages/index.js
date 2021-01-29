e785f50123921c0021f5492220c0589009bde505
Jurrian <jurrianlammerts@hotmail.com>
2021-01-29 11:57:27 +0100

    Fixed some animations

--- pages/index.js
+++ pages/index.js
@@ -1,73 +1,3 @@
-// import { gsap } from "gsap/dist/gsap";
-
-// import { useEffect, useState } from "react";
-// import Layout from "../components/Layout";
-// import Banner from "../components/Banner";
-// import HomeCases from "../components/HomeCases";
-// import IntroOverlay from "../components/IntroOverlay";
-// import TextSection from "../components/TextSection";
-// import CTA from "../components/HomeCTA";
-// import Footer from "../components/Footer";
-
-// import { getSortedCasesData } from "../lib/cases";
-
-// const tl = gsap.timeline();
-
-// const homeAnimation = (completeAnimation) => {
-//   tl.from(".line span", 1.8, {
-//     y: 100,
-//     ease: "power4.out",
-//     delay: 0.75,
-//     skewY: 7,
-//     stagger: {
-//       amount: 0.3,
-//     },
-//   })
-//     .to(".overlay-top", 1.2, {
-//       height: 0,
-//       ease: "expo.inOut",
-//       stagger: 0.4,
-//     })
-//     .to(".App", 0, {
-//       css: { position: "relative" },
-//     })
-//     .to(".intro-overlay", 0, {
-//       css: { display: "none" },
-//       onComplete: completeAnimation,
-//     });
-// };
-
-// export default function Home({ allCases }) {
-//   const [animationComplete, setAnimationComplete] = useState(false);
-
-//   const completeAnimation = () => {
-//     setAnimationComplete(true);
-//   };
-
-//   useEffect(() => {
-//     homeAnimation(completeAnimation);
-//   }, []);
-
-//   return (
-//     <Layout>
-//       {!animationComplete && <IntroOverlay />}
-//       <Banner />
-//       <HomeCases cases={allCases} />
-//       <TextSection />
-//       <CTA />
-//       <Footer />
-//     </Layout>
-//   );
-// }
-
-// export async function getStaticProps() {
-//   const allCases = getSortedCasesData();
-
-//   return {
-//     props: { allCases },
-//   };
-// }
-
 import { gsap } from "gsap/dist/gsap";
 
 import { useEffect, useState } from "react";
@@ -85,9 +15,9 @@ const tl = gsap.timeline();
 
 const homeAnimation = (completeAnimation) => {
   tl.from(".line span", 1.8, {
-    y: 100,
+    y: 140,
     ease: "power4.out",
-    delay: 0.75,
+    delay: 1,
     skewY: 7,
     stagger: {
       amount: 0.3,
@@ -98,6 +28,15 @@ const homeAnimation = (completeAnimation) => {
       ease: "expo.inOut",
       stagger: 0.4,
     })
+    .from(
+      ".case-image img",
+      1,
+      {
+        ease: "expo.inOut",
+        scale: 1.25,
+      },
+      "-=1"
+    )
     .to(".App", 0, {
       css: { position: "relative" },
     })
