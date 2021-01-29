bf33844974ca1e7ec5c07598b695b5a0f3dac259
Jurrian <jurrianlammerts@hotmail.com>
2021-01-29 11:38:12 +0100

    Small fix

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,13 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;600;700&amp;display=swap|Roboto:wght@200;300&amp;display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
