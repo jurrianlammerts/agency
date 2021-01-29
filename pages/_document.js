7d0741b2285da00e5d2656747ac12ba2a9cf577c
Jurrian <jurrianlammerts@hotmail.com>
2021-01-29 11:41:45 +0100

    Changed back

--- pages/_document.js
+++ pages/_document.js
@@ -4,13 +4,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;600;700&amp;display=swap|Roboto:wght@200;300&amp;display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
