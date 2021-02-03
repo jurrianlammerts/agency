26feedd031e5b251bd30f702efccce3cce1540bb
Jurrian <jurrianlammerts@hotmail.com>
2021-02-03 13:02:00 +0100

    Changed vh to 100% mobile hack

--- components/Layout.js
+++ components/Layout.js
@@ -29,6 +29,10 @@ export default function Layout({ dark, children }) {
       height: window.innerHeight,
       width: window.innerWidth,
     });
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    const vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
   }, []);
 
   useEffect(() => {
